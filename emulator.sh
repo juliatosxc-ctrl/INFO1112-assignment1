26b114384c6f479c851836300beb2e1a697bd2b9
juliatosxc-ctrl <juliatosxc@gmail.com>
2026-09-15 12:28:53 +1000

    Add files via upload

---
+++ emulator.sh
@@ -0,0 +1,2 @@
+#that models the hypothetical VSC. It takes a .bin file as input and executes/carries out the (binary) instructions contained in that .bin file
+# caller ->  $ bash emulator . sh sample . bin
